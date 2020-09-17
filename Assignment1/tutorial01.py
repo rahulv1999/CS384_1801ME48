5c667ffc7bb9c74b7d94ec67b4a8ffa1dbe5e039
rahulve1999 <rahulv91199@gmail.com>
2020-09-17 15:13:14 +0530

    added error message in add and sub and other fns

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -1,12 +1,18 @@
 # Function to add two numbers 
-def add(num1, num2): 
-	addition = num1 + num2
-	return addition
+def add(num1, num2):
+    addition = num1 + num2
+    if type(addition) != float and type(addition) != int:
+        addition = 0 
+        print("not valid inputs for addition")
+    return addition
 
 # Function to subtract two numbers 
 def subtract(num1, num2): 
-	subtraction = num1 - num2
-	return subtraction
+    subtraction = num1 - num2
+    if type(subtraction) != float and type(subtraction) != int:
+        subtraction = 0 
+        print("not valid inputs for subtraction")    
+    return subtraction
 
 # Function to multiply two numbers 
 def multiply(num1, num2): 
@@ -16,6 +22,7 @@ def multiply(num1, num2):
     except: 
         print("error in multiplication")
         multiplication =0
+    multiplication = round(multiplication,3)
     return multiplication
 
 # Function to divide two numbers 
@@ -39,10 +46,19 @@ def divide(num1, num2):
 #You cant use the inbuilt python function x ** y . Write your own function
 def power(num1, num2): #num1 ^ num2
 	#DivisionLogic 
+    if float(num2) != float(int(num2)):
+        print("numn2 can only be a whole number")
+        power = 0
+        return power
     try:
         power = 1
-        for i in range(int(num2)):
-            power *= num1
+        if num2>0:
+            for i in range(int(num2)):
+                power *= num1
+        else:
+            for i in range(int(abs(num2))):
+                power /= num1            
+        power = round(power,3)
     except :
         print("error in power")
         power = 0
@@ -59,13 +75,13 @@ def power(num1, num2): #num1 ^ num2
 def printGP(a, r, n): 
     gp=[]
     try:
-        if n>0:
+        if n>0 and float(n) != float(int(n)):
             gp.append(round(a,3))
-            for i in range(n-1):
+            for i in range(int(n)-1):
                 a *= r
                 gp.append(round(a,3)) 
         else:
-            print("error in printGp n should be >0")
+            print("error in printGp n should be >0  and a whole number ")
             gp.append(0)
     except:
         print("error in printGp")
@@ -79,13 +95,13 @@ def printGP(a, r, n):
 def printAP(a, d, n):
     ap =[]
     try:
-        if n>0:
+        if n>0 and float(n) != float(int(n)):
             ap.append(a)
-            for i in range(n-1):
+            for i in range(int(n)-1):
                 a +=d
                 ap.append(a)
         else:
-            print("error in printAp n should be >0")
+            print("error in printAp n should be >0 and a whole number ")
             ap.append(0)           
     except :
         print("error in printAp")
@@ -93,21 +109,24 @@ def printAP(a, d, n):
         ap.append(0)
     return ap
 
-
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
 def printHP(a, d, n):
     hp = []
     try:
-        if n>0:
+        if n>0 and float(n) != float(int(n)):
             
-            for i in range(n):
+            for i in range(int(n)):
                 if a!=0:
                     hp.append(round(1/a,3))
                     a +=d
                 else:
                     print(f'{i+1} value of term is becoming 1/0 not allowed')
+                    hp.clear
+                    hp.append(0)
                     break
         else:
-            print("error in printHp n should be >0")
+            print("error in printHp n should be >0 and a whole number")
             hp.append(0)           
     except :
         print("error in printHp")
