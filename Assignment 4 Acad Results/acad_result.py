322ce4c0a94fe7c8a560b40f1318799f5a96cb23
rahulve1999 <rahulv91199@gmail.com>
2020-11-05 19:10:03 +0530

    completed making rollno_overall.csv for all students

--- Assignment 4 Acad Results/acad_result.py
+++ Assignment 4 Acad Results/acad_result.py
@@ -24,8 +24,8 @@ grade = {'AA':10,
          'I' :0
           }
 
-
-
+columns = ['Sem','Subject','Credits','Grade','Type']
+#for roll_individual
 for i,roll in enumerate(df.roll):
     name = os.path.join(path,f'{roll}_individual.csv')
     if not os.path.exists(name):
@@ -33,14 +33,29 @@ for i,roll in enumerate(df.roll):
         csvw = csv.writer(f)
         csvw.writerow([f"Roll: {roll}"])
         csvw.writerow([f'Semester Wise Details'])
-        csvw.writerow(['Sem','Subject','Credits','Grade','Type'])
+        csvw.writerow(columns)
         f.close()
     row = list(df.values[i])
     row.pop(0)
+    if row[-2] not in list(grade.keys()):
+        print(f'in misc.csv {i}')
+        name = os.path.join(path,'misc.csv')
+        if not os.path.exists(name):
+           f = open(name,'w',newline='')
+           csvw = csv.writer(f)
+           csvw.writerow(["Misc values"])
+           csvw.writerow(columns)
+           f.close() 
+        with open(name,'a',newline='') as f:
+            csvw = csv.writer(f)
+            csvw.writerow(row)
+        continue
+
     try:
         with open(name,'a',newline='') as f:
             csvw = csv.writer(f)
             csvw.writerow(row)
+
     except:
         print(f'in misc.csv {i}')
         name = os.path.join(path,'misc.csv')
@@ -48,7 +63,7 @@ for i,roll in enumerate(df.roll):
            f = open(name,'w',newline='')
            csvw = csv.writer(f)
            csvw.writerow(["Misc values"])
-           csvw.writerow(['Sem','Subject','Credits','Grade','Type'])
+           csvw.writerow(columns)
            f.close() 
         with open(name,'a',newline='') as f:
             csvw = csv.writer(f)
@@ -57,4 +72,69 @@ for i,roll in enumerate(df.roll):
     
 
     
-    
+#for rollno_overall.csv this depends upon above code 
+
+for file in os.listdir(path):
+    if file=='misc.csv':
+        continue
+    data = []
+    with open(os.path.join(path,file),newline='') as csvfile:
+        csvr = csv.reader(csvfile,)
+        skip=3
+        for row in csvr:
+            if skip==0:
+                data.append(row)
+            else:
+                skip = skip -1
+                if skip==0:
+                    head = row
+
+    df = pd.DataFrame(data,columns=head)
+    l = (df.Sem).unique()
+    total_credit = 0
+    total_credit_cleared = 0
+    spi_list =[]
+    sem_credits_list = []
+    newfile = file.split('_')[0] + '_overall.csv'
+    with open(os.path.join(path,newfile),'w',newline='') as csvfile:
+        csvr = csv.writer(csvfile)
+        rno = file.split('_')[0]
+        csvr.writerow([f'Roll: {rno}'])   
+        csvr.writerow(['Semester','Semester Credits','Semester Credits Cleared','SPI','Total Credits','Total Credits Cleared','CPI'])
+        for sem in l:
+            s = df[df.Sem ==sem]
+            sem_credit = 0
+            sem_credit = [sem_credit :=sem_credit + int(i) for i in s.Credits][-1]
+            total_credit = total_credit + int(sem_credit)
+            cleared_credits = 0
+            for grades,credits in zip(s.Grade,s.Credits):
+                if grades in list(grade.keys()) :
+                    if grade[grades]>0:
+                        cleared_credits = cleared_credits + int(credits)
+                else:
+                    print("Grade error ",grades)
+                    
+            total_credit_cleared = total_credit_cleared + cleared_credits
+            sum_spi = 0
+            for g,c in zip(s.Grade,s.Credits):
+                sum_spi = sum_spi  + int(c) * int(grade[g])
+            
+            spi = sum_spi/sem_credit
+            spi_list.append(spi)
+            sem_credits_list.append(sem_credit)
+            cpi_sum = 0
+            for c,g in zip(sem_credits_list,spi_list):
+                cpi_sum = cpi_sum + c*g
+            
+            cpi = cpi_sum/sum(sem_credits_list)
+            csvr.writerow([sem,sem_credit,cleared_credits,round(spi,2),total_credit,total_credit_cleared,round(cpi,2)])
+
+
+
+
+        
+        
+
+
+
+
