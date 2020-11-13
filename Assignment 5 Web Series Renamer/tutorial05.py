bfe2088e91a2f369cd30f7421cf0d03dd092d07f
rahulve1999 <rahulv91199@gmail.com>
2020-11-13 21:31:58 +0530

    assignment 5 complted!!!

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -4,25 +4,40 @@ import re
 e_padding = 2
 s_padding = 2
 
+
 def rm_zero(s):
+    '''
+    Fn to remove leading zero in numbers
+    '''
     s = s.strip()
-    if s[0]=='0':
-        for i in range(1,len(s)):
-            if s[i] !='0':
-                return s[i:]
-    else:
-        return s
+    for i in range(len(s)):
+        if s[i] !='0':
+            return s[i:]
 
 
 #considering folder in current dir
-def rename_FIR(folder_name):
-    # path = os.path.join(os.getcwd(),os.path.join('Subtitles',folder_name))
-    # for file in os.listdir(path):
-    #     name = file.split('-')
-    #     ext = name[-1].split('.')[-1]
-    #     episode =
-    #     head = "FIR - "  + name
-    pass
+def rename_FIR(folder_name = 'FIR'):
+    path = os.path.join(os.getcwd(),os.path.join('Subtitles',folder_name))
+    try:
+        for file in os.listdir(path):
+            name = file.split('-')
+            name = [i.strip() for i in name]
+            # print(file)
+            i = ''
+            for s in name[1:]:
+                if 'Episode' in s:
+                    i = s
+                    break
+            episode = (e_padding - len(rm_zero(i.split()[1])))*'0' + rm_zero(i.split()[1])
+            ext = name[-1].split('.')[-1]
+            file_name = name[0] +" Episode " + episode + "." + ext
+            file_name_old = os.path.join(path,file)
+            file_name = os.path.join(path,file_name)
+            os.rename(file_name_old,file_name)
+    except:
+        pass
+
+
 
 
 def rename_Game_of_Thrones(folder_name='Game of Thrones'):
@@ -69,9 +84,28 @@ def rename_Sherlock(folder_name = 'Sherlock'):
 
 
 
-# def rename_How_I_Met_Your_Mother(folder_name):
-#     # rename Logic
-#     pass
+def rename_How_I_Met_Your_Mother(folder_name = 'How I Met Your Mother'):
+    path = os.path.join(os.getcwd(),os.path.join('Subtitles',folder_name))
+    for file in os.listdir(path):
+        try:
+            name = file.split('-')
+            name = [i.strip() for i in name]
+            season = (s_padding - len(rm_zero(name[1].split('x')[0])))*'0' + rm_zero(name[1].split('x')[0])
+            episode = (e_padding - len(rm_zero(name[1].split('x')[1])))*'0' +rm_zero(name[1].split('x')[1])
+            if len(name)==3:
+                episode_name = name[-1].split('.')[0].strip()
+            else:
+                episode_name = name[-2].split('.')[0].strip()
+            ext = name[-1].split('.')[-1]
+            file_name = name[0] + " - Season " + season + " Episode " + episode + " - " + episode_name + "." + ext
+            file_name_old = os.path.join(path,file)
+            file_name = os.path.join(path,file_name)
+            try:
+                os.rename(file_name_old,file_name)
+            except:
+                os.remove(file_name_old)
+        except:
+            pass
 
 
 
@@ -102,4 +136,3 @@ def rename_Suits(folder_name = 'Suits'):
 
 
 
-rename_Game_of_Thrones()
